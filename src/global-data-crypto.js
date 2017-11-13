28aa7a22ad9ea13e65d442bd0c3e08652ebc6401
Grem25 <jeremie.zanone@heig-vd.ch>
2017-11-13 17:39:15 +0100

    code reformat eslint

--- src/global-data-crypto.js
+++ src/global-data-crypto.js
@@ -1,9 +1,10 @@
 const request = require('superagent');
 const json2csv = require('json2csv');
-const unixDate = require('unix-time')
+const unixDate = require('unix-time');
+
 const fs = require('fs');
 
-const startYear = 2014;
+const startYear = 2017;
 const startMonth = 1;
 const startDay = 1;
 
@@ -13,48 +14,38 @@ const endDay = 4;
 
 const dataCurrency = 'DASH';
 
-let item = [];
-let data = {};
+const item = [];
+
+function jsontocsv(data) {
+  const fields = ['currency', 'price', 'date'];
+
+  data.forEach((element) => {
+    console.log(element);
+  }, this);
+  const csv = json2csv({ data, fields });
+  fs.writeFile(`raw_${dataCurrency}.csv`, csv, (err) => {
+    if (err) throw err;
+    console.log('file saved');
+  });
+}
 
 function fetchData(startDate, endDate) {
-
-  let timestampInt = unixDate(startDate);
-
+  const timestampInt = unixDate(startDate);
   const targetUrl = `https://min-api.cryptocompare.com/data/pricehistorical?fsym=${dataCurrency}&tsyms=USD,&ts=${timestampInt}`;
-  let repos = [];
-  
   request
     .get(targetUrl)
     .end((err, res) => {
-      let data = {};
+      const data = {};
       data.currency = dataCurrency;
       data.price = res.body.DASH.USD;
       data.date = startDate;
       item.push(data);
-      if (startDate.getTime() == endDate.getTime()){
+      if (startDate.getTime() === endDate.getTime()) {
         jsontocsv(item);
-      }else{ 
-        fetchData(new Date(startDate.setDate(startDate.getDate() + 1)), endDate);;
+      } else {
+        fetchData(new Date(startDate.setDate(startDate.getDate() + 1)), endDate);
       }
-     
-
     });
-}
-
-function jsontocsv(data){
-  var fields = ['currency', 'price', 'date'];
-
-  data.forEach(function(element) {
-    console.log(element);
-    
-  }, this);
-  let csv = json2csv({ data: data, fields: fields });
-  
-  fs.writeFile(`raw_${dataCurrency}.csv`, csv, function(err) {
-    if (err) throw err;
-    console.log('file saved');
-  });
-
 }
 
 fetchData(new Date(startYear, startMonth - 1, startDay), new Date(endYear, endMonth - 1, endDay));
